2c06c1e8ed9b38d451f7c93955ddf2cd8ad46dca
Leon Gurin <leon9gur@gmail.com>
2022-03-13 22:21:38 +0200

    ++

--- commands/score.ts
+++ commands/score.ts
@@ -7,8 +7,21 @@ export default {
 
     permissions: [],
 
-    callback: ({ message }) => {
-        message.reply(' אתה תקבל ' + Math.floor(Math.random()*100) + ' במבחן הקרוב... ');        
+    callback: async ({ message }) => {
+        let random = Math.floor(Math.random()*100);
+        const msg = await message.reply(' אתה תקבל ' + random + ' במבחן הקרוב... ');
+        if(random < 25){
+            msg.react('<:_omegalolgadi:811854297854640138>');
+        }
+        else if(random < 50){
+            msg.react('<:gadi_stickman:829752119597989919>');
+        }
+        else if(random < 75){
+            msg.react('<:_gadi_happy_b:811853254120046633>');
+        }
+        else{
+            msg.react('<:_gadi_wow:811855140117282816>');
+        }
     }
 } as ICommand
 
--- commands/summ.ts
+++ commands/summ.ts
@@ -12,9 +12,27 @@ var ilanArr: string[] = [
     'https://cs.haifa.ac.il/~yuri/Ilan60/ilan.png'
 ];
 
+var yaelArr: string[] = [
+    'https://www.reali.org.il/wp-content/uploads/2021/08/Yael-200x300.jpg',
+];
+
+var danArr: string[] = [
+    'https://cs.hevra.haifa.ac.il/images/2021/06/07/pxl_20210601_105840663.portrait1.jpg',
+    'https://danrsm.github.io/dan_rosenbaum.png',
+    'https://pbs.twimg.com/profile_images/756365601925369856/9kKCMIdl_400x400.jpg',
+];
+
+var orArr: string[] = [
+    'https://cs.hevra.haifa.ac.il/images/staff/Academic/or_meir.jpg',
+];
+
+var daniArr: string[] = [
+    'https://www.cs.haifa.ac.il/~dkeren/danny.jpg',
+];
+
 var random: string;
 
-const lecturerArr: string[] = ["gadi", "ilan"];
+const lecturerArr: string[] = ["gadi", "ilan","yael","dan","or","dani"];
 
 export default {
     category: 'Summon',
@@ -44,6 +62,35 @@ export default {
                 .setDescription("summoned Ilan")
                 .setImage(random)
                 return embed;
+            case 'yael':
+                random = yaelArr[Math.floor(Math.random() * yaelArr.length)]
+                // const embed = new MessageEmbed()
+                embed.setTitle("YAEL")
+                .setDescription("summoned Yael")
+                .setImage(random)
+                return embed;
+            case 'dan':
+                random = danArr[Math.floor(Math.random() * danArr.length)]
+                // const embed = new MessageEmbed()
+                embed.setTitle("DAN")
+                .setDescription("summoned Dan")
+                .setImage(random)
+                return embed;
+            case 'or':
+                random = orArr[Math.floor(Math.random() * orArr.length)]
+                // const embed = new MessageEmbed()
+                embed.setTitle("OR")
+                .setDescription("summoned Or")
+                .setImage(random)
+                return embed;
+            case 'dani':
+                random = daniArr[Math.floor(Math.random() * daniArr.length)]
+                // const embed = new MessageEmbed()
+                embed.setTitle("DANI")
+                .setDescription("summoned Dani")
+                .setImage(random)
+                return embed;
+
             default:
                 message.reply("Please specify a lecturer.\nType the command: '!summ' + space + lecturer name.\nThe currently available lecturers are: \n*" + lecturerArr.join(', ') + "*");
         }        
