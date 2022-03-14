d2f1e8bac97368a77ebdad08d716de5e1dfe7d16
Leon Gurin <leon9gur@gmail.com>
2022-03-14 19:19:54 +0200

    organized quotes and added indexing feature

--- commands/quote.ts
+++ commands/quote.ts
@@ -1,12 +1,13 @@
 import { ICommand } from "wokcommands";
 
 var quotes: string[] = [
-    'זה מאוד חשוב לתת לילד שלך למות',
-    'אז בסופו של דבר זה בעצם כלב',
-    'מילים נוצצות זה כמו תוכי בגן חיות, רק יופי חיצוני',
-    'פסדר? זה פסדר??',
-    'עוף ברוטב מרינדת תפוזים',
-    'אחד זייף',
+    '1) זה מאוד חשוב לתת לילד שלך למות',
+    '2) אז בסופו של דבר זה בעצם כלב',
+    '3) מילים נוצצות זה כמו תוכי בגן חיות, רק יופי חיצוני',
+    '4) פסדר? זה פסדר??',
+    '5) עוף ברוטב מרינדת תפוזים',
+    '6) אחד זייף',
+    '7) לא אומרים לא על C אומרים Yes daddy more',
     
 ];
 
@@ -19,13 +20,26 @@ export default {
 
     callback: ({ message }) => {
         let words = message.content.split(' ');
-        switch (words[1]) {
-            case '-all':
-                message.channel.send("The currently available quotes are: \n\n*" + quotes.join(',\n') + "*\n\nDM me if you have a quote you want to add!");
-                break;
-            default:
-                message.reply("*" + quotes[Math.floor(Math.random()*quotes.length)] + "*");
-        }       
+        if(words[1] == "-all"){
+            message.reply("The currently available quotes are: \n\n*" + quotes.join(',\n') + "*\n\nDM me if you have a quote you want to add!");
+        }
+        else if(words[1] != undefined){
+            message.reply("*" + quotes[parseInt(words[1])+1] + "*");
+        }
+        else{
+            message.reply("*" + quotes[Math.floor(Math.random() * quotes.length)] + "*");
+        }
+
+        // switch (words[1]) {
+        //     case '-all':
+        //         message.channel.send("The currently available quotes are: \n\n*" + quotes.join(',\n') + "*\n\nDM me if you have a quote you want to add!");
+        //         break;
+        //     case "\n":
+        //         message.reply("*" + quotes[Math.floor(Math.random()*quotes.length)] + "*");
+        //         break;
+        //     default:
+        //         message.reply("*" + quotes[parseInt(words[1])] + "*");
+        // }       
     }
 } as ICommand
 
