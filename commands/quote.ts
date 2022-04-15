10649e7788725f1d70fb3cb89b4f794c05105712
Leon Gurin <leon9gur@gmail.com>
2022-04-15 16:04:42 +0300

    added many quotes

--- commands/quote.ts
+++ commands/quote.ts
@@ -9,7 +9,21 @@ var quotes: string[] = [
     '6) אחד זייף',
     '7) לא אומרים לא על C אומרים Yes daddy more',
     "8) Yeah, I'm a nerd, it's thursday evening and were talking about irregular languages, bro",
-    '9) אם דרסת חתול, אתה לא תוכל לדעת אם דרסת אותו אם תסתכל מלמעלה, אתה צריך לזוז צעד אחד קדימה ואז להסתכל'
+    '9) אם דרסת חתול, אתה לא תוכל לדעת אם דרסת אותו אם תסתכל מלמעלה, אתה צריך לזוז צעד אחד קדימה ואז להסתכל',
+    '10) אל תכתבו לי תוכניות אני לא קומפיילר',
+    '11) דף נקי זאת תשובה מאוד קצרה',
+    '12) יש לי תחושה שבארצות הברית המורה הייתה נשמעת אחרת, גם באנגליה. אפילו בניו זילנד...',
+    '13) ?אורן: מה זה סאס',
+    '14) השאלה היא לא אם אתה יכול להגיע השאלה אם אתה יכול לגמור',
+    '15) אם דרסת חתול, אתה לא תוכל לדעת אם דרסת אותו אם תסתכל מלמעלה, אתה צריך לזוז צעד אחד קדימה ואז להסתכל',
+    '16) אל תכתבו לי תוכניות אני לא קומפיילר',
+    '17) אני רואה פרצופים, זה סוג של שמחת חיים',
+    '18) אם תגידו למדתי עם אליאס LC-3 יצחקו עליכם',
+    '19) מחזור 15 אני ממליץ לכם לעשות כל מאמץ לקבל במבוא לחומרה ציון מדהים (90 ומעלה)',
+    '20) ?"אני לא אוהבת את הביטוי הזה, מה זה "ברח לו',
+    '21) יום טוב אור-אל',
+    
+
 ];
 
 export default {
@@ -29,18 +43,7 @@ export default {
         }
         else{
             message.reply("*" + quotes[Math.floor(Math.random() * quotes.length)] + "*");
-        }
-
-        // switch (words[1]) {
-        //     case '-all':
-        //         message.channel.send("The currently available quotes are: \n\n*" + quotes.join(',\n') + "*\n\nDM me if you have a quote you want to add!");
-        //         break;
-        //     case "\n":
-        //         message.reply("*" + quotes[Math.floor(Math.random()*quotes.length)] + "*");
-        //         break;
-        //     default:
-        //         message.reply("*" + quotes[parseInt(words[1])] + "*");
-        // }       
+        }  
     }
 } as ICommand
 
