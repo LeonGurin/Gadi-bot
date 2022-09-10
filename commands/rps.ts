5683de1a197afa2e046c3091f2c8afc077ede009
Omer Esco <88593577+OmerEsco@users.noreply.github.com>
2022-09-11 02:04:26 +0300

    Update rps.ts

--- commands/rps.ts
+++ commands/rps.ts
@@ -8,23 +8,16 @@ export default {
     permissions: [],
 
     callback: ({ message }) => {
-        let random = Math.floor(Math.random()*3);
-        let words = message.content.split(' ');
+        let options = ["rock", "paper", "scissors"]
+        let random = Math.floor(Math.random() * 3)
+        let words = message.content.split(' ')
         let choice;
-        switch(words[1]) {
-            case "rock":
-                choice = 0;
-                break;
-            case "paper":
-                choice = 1;
-                break;
-            case "scissors":
-                choice = 2;
-                break;
-            default:
-                message.reply("Invalid choice, dumbass")
-                return
+        if ((choice = options.indexOf(words[1])) < 0) {
+            message.reply("Invalid choice, dumbass")
         }
+        // print choices
+        message.reply("**I Chose**: " + options[random])
+        message.reply("**You Chose**: " + words[1])
         // draw
         if (choice == random) {
             message.reply("Draw, you sussy baka")
