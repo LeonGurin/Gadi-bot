a80c74c7a144a67150a46f1af675d9eb6b3ad19c
Leon Gurin <leon9gur@gmail.com>
2022-09-11 01:37:45 +0300

    +++

--- commands/nir.ts
+++ commands/nir.ts
@@ -1,24 +1,26 @@
-import { MessageEmbed } from "discord.js";
-import { ICommand } from "wokcommands";
+// import { MessageEmbed } from "discord.js";
+// import { ICommand } from "wokcommands";
 
-let nirID = '680782833094361195';
+// let nirID = '680782833094361195';
 
-export default {
-    category: 'nir',
-    name: "nir",
-    description: "nir",
+// export default {
+//     category: 'Nir',
+//     name: "nir",
+//     description: "nir",
 
-    permissions: [],
+//     permissions: [],
 
-    callback: async ({ message }) => {
-        if(message.user.id == nirID) {
-            const msg = await message.reply('@Nir#1579 What is the password?');
-            
-            return 
-        }
+//     callback: async ({ message }) => {
+//         if(message.user.id == nirID) {
+//             const msg = await message.reply('@Nir#1579 What is the password?');
+//             if(msg.content == 'sussy baka') {
+//                 message.reply('You are now verified!');
+//             }
+//             return 
+//         }
 
-        }
+//         }
         
-    }
-} as ICommand
+//     }
+// } as ICommand
 
---
+++ commands/rps.ts
@@ -0,0 +1,41 @@
+import { ICommand } from "wokcommands";
+
+export default {
+    category: "Rock Paper Scissors",
+    name: "rps",
+    description: "Starts a session of rock-paper-scissors against Gadi-Bot.",
+
+    permissions: [],
+
+    callback: ({ message }) => {
+        let random = Math.floor(Math.random()*3);
+        let words = message.content.split(' ');
+        let choice;
+        switch(words[1]) {
+            case "rock":
+                choice = 0;
+                break;
+            case "paper":
+                choice = 1;
+                break;
+            case "scissors":
+                choice = 2;
+                break;
+            default:
+                message.reply("Invalid choice, dumbass")
+                return
+        }
+        // draw
+        if (choice == random) {
+            message.reply("Draw, you sussy baka")
+        }
+        // you lost 
+        else if ((choice == 0 && random == 1) || (choice == 1 && random == 2) || (choice == 2 && random == 0)) {
+            message.reply("You lost! Muhahaha (Failed Data Structures)");
+        }
+        // you won
+        else {
+            message.reply("You win... but you probably cheated");
+        }
+    }
+} as ICommand
