3c6b5536d4dd6e950422e9244906e921892832ad
Omer Esco <88593577+OmerEsco@users.noreply.github.com>
2022-09-12 18:06:20 +0300

    Update rps.ts

--- commands/rps.ts
+++ commands/rps.ts
@@ -1,64 +1,79 @@
 import { MessageEmbed } from "discord.js";
 import { ICommand } from "wokcommands";
 
+function createEmbed(title: string, description: string) {
+  let embed = new MessageEmbed();
+  embed.setTitle(title);
+  embed.setDescription(description);
+  return embed;
+}
+
 export default {
-    category: "Rock Paper Scissors",
-    name: "rps",
-    description: "Starts a session of rock-paper-scissors against Gadi-Bot.",
+  category: "Rock Paper Scissors",
+  name: "rps",
+  description: "Starts a session of rock-paper-scissors against Gadi-Bot.",
 
-    permissions: [],
+  permissions: [],
 
-    callback: ({ message }) => {
-        let embed = new MessageEmbed()
-        let title = ""
-        let description = ""
-        let choices = ["rock", "paper", "scissors"]
-        let random = Math.floor(Math.random() * 3)
-        let words = message.content.split(' ')
-        // options
-        let help = (words[1] == "-h")
-        let rigged = (words[1] == "-r")
-        let god = (words[1] == "-g")
-        // help option
-        if (help) {
-            title = "HELP"
-            description = "**Input:** rock/paper/scissors. Other inputs are invalid.\n" +
-                          "**Options:**\n" +
-                          "`-r`: rigged - you always win.\n"
-            embed.setTitle(title)
-            embed.setDescription(description)
-            return embed
-        }
-        // invalid input
-        let choice;
-        if ((choice = choices.indexOf(words[words.length - 1])) < 0) {
-            embed.setTitle("IDIOT")
-            .setDescription("Invalid choice, dumbass")
-            return embed
-        }
-        // print choices
-        description += "**I Chose:** " + choices[random] + "\n"
-        description += "**You Chose:** " + words[words.length - 1] + "\n"
-        description += "**Result:** "
-        // you won
-        if ((choice == 1 && random == 0) || (choice == 2 && random == 1) || (choice == 0 && random == 2) || rigged) {
-            title = "VICTORY"
-            description += "You win... but you probably cheated"
-        }
-        // draw
-        else if (choice == random) {
-            title = "DRAW"
-            description += "Draw, you sussy baka"
-        }
-        // you lost
-        else {
-            title = "DEFEAT"
-            description += "You lost! Muhahaha (Failed Data Structures)"
-        }
-        // set attributes of embed
-        embed.setTitle(title)
-        .setDescription(description)
-        return embed
+  callback: ({ message }) => {
+    let title = "";
+    let description = "";
+    let choices = ["rock", "paper", "scissors"];
+    let user_choice;
+    let bot_choice = Math.floor(Math.random() * 3);
+    let words = message.content.split(" ");
+    let rigged = false;
+    // help option
+    if ("-h" in words) {
+      title = "HELP";
+      description =
+        "```rps [OPTION]... [INPUT]...```\n" +
+        "**Input:** rock/paper/scissors.\n" +
+        "Other inputs are invalid.\n" +
+        "**Options:**\n" +
+        "`-r`: rigged, user always wins (cheater).\n" +
+        "`-g`: enables god choice (god beats everything except itself).";
+      return createEmbed(title, description);
     }
-} as ICommand
-
+    // rigged option
+    if ("-r" in words) {
+      rigged = true;
+    }
+    // god option
+    if ("-g" in words) {
+      choices.push("god");
+    }
+    // check user input
+    if ((user_choice = choices.indexOf(words[words.length - 1])) < 0) {
+      title = "IDIOT";
+      description = "Invalid data, dumbass.";
+      return createEmbed(title, description);
+    }
+    // print user and bot choices
+    description += "**I Chose:** " + choices[bot_choice] + "\n";
+    description += "**You Chose:** " + words[words.length - 1] + "\n";
+    description += "**Result:** ";
+    // user wins scenario
+    if (
+      (user_choice == 1 && bot_choice == 0) || // paper beats rock
+      (user_choice == 2 && bot_choice == 1) || // scissors beat paper
+      (user_choice == 0 && bot_choice == 2) || // rock beats scissors
+      (user_choice == 3 && bot_choice != 3) || // god beats everything except itself
+      rigged
+    ) {
+      title = "VICTORY";
+      description += "You win... but you probably cheated";
+    }
+    // draw scenario
+    else if (user_choice == bot_choice) {
+      title = "DRAW";
+      description += "Draw, you sussy baka";
+    }
+    // user loses scenario
+    else {
+      title = "DEFEAT";
+      description += "You lost! Muhahaha (Failed Data Structures)";
+    }
+    return createEmbed(title, description);
+  },
+} as ICommand;
