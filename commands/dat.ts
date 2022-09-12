28fb65d5058ea084987b5ef10c2241030905d3d9
Omer Esco <88593577+OmerEsco@users.noreply.github.com>
2022-09-13 00:56:03 +0300

    🧮

--- commands/dat.ts
+++ commands/dat.ts
@@ -54,7 +54,7 @@ export default {
           "." +
           date.getFullYear().toString()
       );
-      msg.react(":calender:");
+      msg.react("📅");
     }
     // print time
     if (print_time) {
@@ -66,7 +66,7 @@ export default {
           ":" +
           date.getSeconds().toString()
       );
-      msg.react(":clock:");
+      msg.react("⌚");
     }
   },
 } as ICommand;
