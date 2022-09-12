fed9d0ba8a6ca663d1b2fb6c201318218c3bc94b
Omer Esco <88593577+OmerEsco@users.noreply.github.com>
2022-09-12 22:12:05 +0300

    Update rps.ts

--- commands/rps.ts
+++ commands/rps.ts
@@ -21,20 +21,21 @@ export default {
     let choices = ["rock", "paper", "scissors"];
     let emojis = [":rock:", ":roll_of_paper:", ":scissors:"];
     let words = message.content.split(" ");
+    let rigged = false;
     // help option
     if (words.indexOf("-h") > 0) {
       title = "HELP";
       description =
-        "```rps [OPTION]... [INPUT]...```\n" +
+        "```!rps [OPTION]... [INPUT]...```\n" +
         "**INPUT:** rock/paper/scissors.\n" +
-        "other inputs are invalid.\n" +
+        "\tother inputs are invalid.\n" +
         "**OPTIONS:**\n" +
-        "`-r`: rigged, user always wins (cheater).\n" +
-        "`-g`: enables god choice (god beats everything except itself).";
+        "`-r`: rigged, you always win (cheater)\n" +
+        "`-g`: god, enables god choice (god beats everything except itself)\n" +
+        "`-e`: extended, adds lizard and spock (nerd mode)";
       return createEmbed(title, description);
     }
     // rigged option
-    let rigged = false;
     if (words.indexOf("-r") > 0) {
       rigged = true;
     }
@@ -43,31 +44,52 @@ export default {
       choices.push("god");
       emojis.push(":angel:");
     }
-    // receive and check user input
-    let bot_choice = Math.floor(Math.random() * choices.length);
-    let user_choice;
-    if ((user_choice = choices.indexOf(words[words.length - 1])) < 0) {
+    // extended option
+    if (words.indexOf("-e") > 0) {
+      choices.push("lizard");
+      choices.push("spock");
+      emojis.push(":lizard:");
+      emojis.push(":vulcan:");
+    }
+    // randomly generate bot choice
+    let bot_choice = choices[Math.floor(Math.random() * choices.length)];
+    // get and check user choice
+    let user_choice = words[words.length - 1];
+    if (choices.indexOf(user_choice) < 0) {
       title = "IDIOT";
-      description = "Invalid data, dumb ass";
+      description = "Invalid input, dumb ass";
       return createEmbed(title, description);
     }
-    // print user and bot choices
+    // print user choice
     description +=
-      "**I CHOSE:** " + choices[bot_choice] + " " + emojis[bot_choice] + "\n";
+      "**I CHOSE:** " +
+      bot_choice +
+      " " +
+      emojis[choices.indexOf(bot_choice)] +
+      "\n";
+    // print bot choice
     description +=
       "**YOU CHOSE:** " +
-      choices[user_choice] +
+      user_choice +
       " " +
-      emojis[user_choice] +
+      emojis[choices.indexOf(user_choice)] +
       "\n";
+    // print result
     description += "**RESULT:** ";
     // user wins scenario
     if (
-      (user_choice == 1 && bot_choice == 0) || // paper beats rock
-      (user_choice == 2 && bot_choice == 1) || // scissors beat paper
-      (user_choice == 0 && bot_choice == 2) || // rock beats scissors
-      (user_choice == 3 && bot_choice != 3) || // god beats everything except itself
-      rigged
+      (user_choice == "rock" &&
+        (bot_choice == "scissors" || bot_choice == "lizard")) || // rock beats scissors and lizard
+      (user_choice == "paper" &&
+        (bot_choice == "rock" || bot_choice == "spock")) || // paper beats rock and spock
+      (user_choice == "scissors" &&
+        (bot_choice == "paper" || bot_choice == "lizard")) || // scissors beat paper and lizard
+      (user_choice == "lizard" &&
+        (bot_choice == "paper" || bot_choice == "spock")) || // lizard beats paper and spock
+      (user_choice == "spock" &&
+        (bot_choice == "rock" || bot_choice == "scissors")) || // spock beats rock and scissors
+      (user_choice == "god" && bot_choice != "god") || // god beats everything except itself
+      rigged // if the game is rigged, the user always wins
     ) {
       title = "VICTORY";
       description += "You win... but you probably cheated";
