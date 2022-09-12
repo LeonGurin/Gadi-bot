ff38f0147ad03b54b035d9f380fe318a69067b3b
Omer Esco <88593577+OmerEsco@users.noreply.github.com>
2022-09-13 00:25:42 +0300

    Update time.ts 😎

--- commands/time.ts
+++ commands/time.ts
@@ -1,3 +1,4 @@
+import { MessageEmbed } from "discord.js";
 import { ICallbackObject, ICommand } from "wokcommands";
 
 export default {
@@ -9,6 +10,52 @@ export default {
 
   callback: ({ message }) => {
     let date = new Date();
-    message.reply(date.getDate().toString());
+    let words = message.content.split(" ");
+    let print_date = true;
+    let print_time = false;
+    // help option
+    if (words.indexOf("-h") > 0) {
+      let embed = new MessageEmbed();
+      let title = "HELP";
+      let description =
+        "```!time [OPTION]...```" +
+        "**OPTIONS:**\n" +
+        "`-d`: only print date\n" +
+        "`-t`: only print time";
+      embed.setTitle(title).setDescription(description);
+      return embed;
+    }
+    // only date option
+    if (words.indexOf("-d") > 0) {
+      print_time = false;
+    }
+    // only time option
+    if (words.indexOf("-t") > 0) {
+      print_date = false;
+    }
+    // print date
+    if (print_date) {
+      message.reply(
+        "Date: " +
+          date.getDay.toString() +
+          " " +
+          date.getDate().toString() +
+          " " +
+          date.getMonth().toString() +
+          " " +
+          date.getFullYear().toString()
+      );
+    }
+    // print time
+    if (print_time) {
+      message.reply(
+        "Time: " +
+          date.getHours().toString() +
+          ":" +
+          date.getMinutes().toString() +
+          ":" +
+          date.getSeconds().toString()
+      );
+    }
   },
 } as ICommand;
